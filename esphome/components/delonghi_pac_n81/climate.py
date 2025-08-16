e4187098a1d54d436223fa404429a4f46169ebdd
gerald.schuster <gerald.schuster@pm.me>
2025-08-16 16:47:12 +0200

    formatting

--- esphome/components/delonghi_pac_n81/climate.py
+++ esphome/components/delonghi_pac_n81/climate.py
@@ -1,3 +1,31 @@
 import esphome.codegen as cg
+from esphome.components import climate, remote_transmitter
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
-delonghi_pac_n81_ns = cg.esphome_ns.namespace("delonghi_pac_n81")
+AUTO_LOAD = ["remote_transmitter"]
+
+delonghi_pac_n81 = cg.esphome_ns.namespace("delonghi_pac_n81")
+DelonghiPacN81Climate = delonghi_pac_n81.class_(
+    "DelonghiPacN81Climate", climate.Climate, cg.Component
+)
+
+DELONGHI_PAC_N81_SCHEMA = cv.Schema(
+    {
+        cv.Required("transmitter"): cv.use_id(
+            remote_transmitter.RemoteTransmitterComponent
+        ),
+    }
+)
+
+CONFIG_SCHEMA = climate.climate_schema(DelonghiPacN81Climate).extend(
+    DELONGHI_PAC_N81_SCHEMA
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+    tx = await cg.get_variable(config["transmitter"])
+    cg.add(var.set_transmitter(tx))
--- esphome/components/delonghi_pac_n81/delonghiPacN81.h
+++ esphome/components/delonghi_pac_n81/delonghiPacN81.h
@@ -12,5 +12,25 @@ namespace delonghi_pac_n81 {
 const uint8_t DELONGHI_TEMP_MIN = 13;  // Celsius
 const uint8_t DELONGHI_TEMP_MAX = 32;  // Celsius
 
+class DelonghiPacN81Climate : public climate::Climate, public Component {
+ public:
+  uint16_t mode;
+  uint16_t fan;
+  const uint16_t address = 0x48;
+  uint16_t temp;
+  uint16_t command;
+  remote_transmitter::RemoteTransmitterComponent *transmitter_{nullptr};
+
+  void setup() override;
+  climate::ClimateTraits traits() override;
+  void control(const climate::ClimateCall &call) override;
+
+  void set_transmitter(remote_transmitter::RemoteTransmitterComponent *tx) { this->transmitter_ = tx; }
+
+ protected:
+  void send_nec_code(uint16_t address, uint16_t command, uint16_t repeats = 1);
+  void send_temp_code(int temp);
+};
+
 }  // namespace delonghi_pac_n81
 }  // namespace esphome
